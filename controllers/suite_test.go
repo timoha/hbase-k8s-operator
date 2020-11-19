0179eead50e8c434abec66fde9c94984f666f7e7
Andrey Elenskiy <andrey.elenskiy@arista.com>
2020-11-19 15:44:48 -0800

    controllers: add e2e tests
    Smoke test to make sure that HBase operator deploys all components.

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -17,13 +17,25 @@ limitations under the License.
 package controllers
 
 import (
+	"context"
+	"errors"
 	"path/filepath"
 	"testing"
+	"time"
 
+	gomock "github.com/golang/mock/gomock"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/tsuna/gohbase/pb"
+	"github.com/tsuna/gohbase/test/mock"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
+	"k8s.io/utils/pointer"
+	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
 	"sigs.k8s.io/controller-runtime/pkg/envtest/printer"
@@ -40,6 +52,8 @@ import (
 var cfg *rest.Config
 var k8sClient client.Client
 var testEnv *envtest.Environment
+var ghClient *mock.MockClient
+var ghAdmin *mock.MockAdminClient
 
 func TestAPIs(t *testing.T) {
 	RegisterFailHandler(Fail)
@@ -49,8 +63,72 @@ func TestAPIs(t *testing.T) {
 		[]Reporter{printer.NewlineReporter{}})
 }
 
+func makeHBaseSpec(confData map[string]string) *hbasev1.HBase {
+	return &hbasev1.HBase{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "hbase",
+			Namespace: "default",
+		},
+		Spec: hbasev1.HBaseSpec{
+			RegionServerSpec: hbasev1.ServerSpec{
+				Count: 3,
+				Metadata: hbasev1.ServerMetadata{
+					Labels: map[string]string{
+						"hbase": "regionserver",
+					},
+				},
+				PodSpec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						corev1.Container{
+							Name: "server",
+							VolumeMounts: []corev1.VolumeMount{
+								corev1.VolumeMount{
+									Name:      "config",
+									MountPath: "/hbase/conf/hbase-site.xml",
+									SubPath:   "hbase-site.xml",
+								},
+							},
+						},
+					},
+				},
+			},
+			MasterSpec: hbasev1.ServerSpec{
+				Count: 2,
+				Metadata: hbasev1.ServerMetadata{
+					Labels: map[string]string{
+						"hbase": "master",
+					},
+				},
+				PodSpec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						corev1.Container{
+							Name: "server",
+							VolumeMounts: []corev1.VolumeMount{
+								corev1.VolumeMount{
+									Name:      "config",
+									MountPath: "/hbase/conf/hbase-site.xml",
+									SubPath:   "hbase-site.xml",
+								},
+							},
+						},
+					},
+				},
+			},
+			Config: hbasev1.ConfigMap{
+				Data: confData,
+			},
+		},
+	}
+}
+
 var _ = BeforeSuite(func(done Done) {
 	logf.SetLogger(zap.LoggerTo(GinkgoWriter, true))
+	gomockCtrl := gomock.NewController(GinkgoT())
+	defer gomockCtrl.Finish()
+	ghClient = mock.NewMockClient(gomockCtrl)
+	ghAdmin = mock.NewMockAdminClient(gomockCtrl)
+	ghAdmin.EXPECT().ClusterStatus().AnyTimes().Return(&pb.ClusterStatus{}, nil)
+	ghAdmin.EXPECT().SetBalancer(gomock.Any()).AnyTimes()
 
 	By("bootstrapping test environment")
 	testEnv = &envtest.Environment{
@@ -67,12 +145,239 @@ var _ = BeforeSuite(func(done Done) {
 
 	// +kubebuilder:scaffold:scheme
 
+	k8sManager, err := ctrl.NewManager(cfg, ctrl.Options{
+		Scheme:    scheme.Scheme,
+		Namespace: "default",
+	})
+	Expect(err).ToNot(HaveOccurred())
+
+	err = (&HBaseReconciler{
+		Client:   k8sManager.GetClient(),
+		Log:      ctrl.Log.WithName("controllers").WithName("HBase"),
+		Scheme:   k8sManager.GetScheme(),
+		GhClient: ghClient,
+		GhAdmin:  ghAdmin,
+	}).SetupWithManager(k8sManager)
+	Expect(err).ToNot(HaveOccurred())
+
+	go func() {
+		err = k8sManager.Start(ctrl.SetupSignalHandler())
+		Expect(err).ToNot(HaveOccurred())
+	}()
+
 	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
 	Expect(err).ToNot(HaveOccurred())
 	Expect(k8sClient).ToNot(BeNil())
 
 	close(done)
 }, 60)
+
+var _ = Describe("HBase controller", func() {
+	var (
+		timeout   = time.Second * 10
+		interval  = time.Millisecond * 250
+		namespace = "default"
+		ctx       = context.Background()
+	)
+
+	Context("When deploying HBase CRD", func() {
+		It("Should deploy all resources", func() {
+			By("By creating a new HBase")
+			hb := makeHBaseSpec(map[string]string{"hbase-site.xml": "conf"})
+			Expect(k8sClient.Create(ctx, hb)).Should(Succeed())
+			hbaseLookupKey := types.NamespacedName{Name: "hbase", Namespace: namespace}
+			createdHBase := &hbasev1.HBase{}
+
+			Eventually(func() error {
+				return k8sClient.Get(ctx, hbaseLookupKey, createdHBase)
+			}, timeout, interval).Should(Succeed())
+
+			By("By checking HBase deployed hbase service")
+			Eventually(func() error {
+				createdService := &corev1.Service{}
+				return k8sClient.Get(ctx, hbaseLookupKey, createdService)
+			}, timeout, interval).Should(Succeed())
+
+			var createdConfigMap *corev1.ConfigMap
+			By("By checking HBase deployed one config map")
+			Eventually(func() (int, error) {
+				configMapList := &corev1.ConfigMapList{}
+				listOpts := []client.ListOption{
+					client.InNamespace(namespace),
+					client.MatchingLabels(map[string]string{"config": "core"}),
+				}
+				if err := k8sClient.List(ctx, configMapList, listOpts...); err != nil {
+					return 0, err
+				}
+				l := len(configMapList.Items)
+				if l != 1 {
+					return l, nil
+				}
+				createdConfigMap = &(configMapList.Items[0])
+				return l, nil
+			}, timeout, interval).Should(Equal(1))
+
+			By("By checking HBase deployed master statefulset")
+			createMasterStatefulSet := &appsv1.StatefulSet{}
+			Eventually(func() error {
+				masterName := types.NamespacedName{Name: "hbasemaster", Namespace: namespace}
+				return k8sClient.Get(ctx, masterName, createMasterStatefulSet)
+			}, timeout, interval).Should(Succeed())
+
+			By("By checking master statefulset has correct number of replicas")
+			Ω(hb.Spec.MasterSpec.Count).Should(Equal(int32(2)))
+
+			By("By checking master statefulset has mounted correct confgmap")
+			vs := createMasterStatefulSet.Spec.Template.Spec.Volumes
+			Ω(len(vs)).Should(Equal(1))
+
+			Ω(vs[0]).Should(Equal(corev1.Volume{
+				Name: "config",
+				VolumeSource: corev1.VolumeSource{
+					ConfigMap: &corev1.ConfigMapVolumeSource{
+						DefaultMode: pointer.Int32Ptr(420),
+						LocalObjectReference: corev1.LocalObjectReference{
+							Name: createdConfigMap.Name,
+						},
+					},
+				},
+			}))
+
+			By("By checking HBase deployed master statefulset has annotation")
+			_, ok := createMasterStatefulSet.Annotations["hbase-controller-revision"]
+			Ω(ok).Should(BeTrue())
+
+			By("By checking HBase deployed regionserver statefulset")
+			createdRegionServerStatefulSet := &appsv1.StatefulSet{}
+			Eventually(func() error {
+				rsName := types.NamespacedName{Name: "regionserver", Namespace: namespace}
+				return k8sClient.Get(ctx, rsName, createdRegionServerStatefulSet)
+			}, timeout, interval).Should(Succeed())
+
+			By("By checking regionserver statefulset has correct number of replicas")
+			Ω(hb.Spec.RegionServerSpec.Count).Should(Equal(int32(3)))
+
+			By("By checking regionserver statefulset has mounted correct confgmap")
+			vs = createdRegionServerStatefulSet.Spec.Template.Spec.Volumes
+			Ω(len(vs)).Should(Equal(1))
+
+			Ω(vs[0]).Should(Equal(corev1.Volume{
+				Name: "config",
+				VolumeSource: corev1.VolumeSource{
+					ConfigMap: &corev1.ConfigMapVolumeSource{
+						DefaultMode: pointer.Int32Ptr(420),
+						LocalObjectReference: corev1.LocalObjectReference{
+							Name: createdConfigMap.Name,
+						},
+					},
+				},
+			}))
+
+			By("By checking regionserver statefulset has annotation")
+			_, ok = createdRegionServerStatefulSet.Annotations["hbase-controller-revision"]
+			Ω(ok).Should(BeTrue())
+		})
+	})
+
+	Context("When updating config of HBase CRD", func() {
+		It("Should redeploy all resources", func() {
+			By("By updating HBase")
+
+			hbaseLookupKey := types.NamespacedName{Name: "hbase", Namespace: namespace}
+			hb := &hbasev1.HBase{}
+			Eventually(func() error {
+				return k8sClient.Get(ctx, hbaseLookupKey, hb)
+			}, timeout, interval).Should(Succeed())
+
+			// get old configmap
+			var existingCms []corev1.ConfigMap
+			Eventually(func() (int, error) {
+				configMapList := &corev1.ConfigMapList{}
+				listOpts := []client.ListOption{
+					client.InNamespace(namespace),
+					client.MatchingLabels(map[string]string{"config": "core"}),
+				}
+				if err := k8sClient.List(ctx, configMapList, listOpts...); err != nil {
+					return 0, err
+				}
+				l := len(configMapList.Items)
+				if l != 1 {
+					return l, nil
+				}
+				existingCms = configMapList.Items
+				return l, nil
+			}, timeout, interval).Should(Equal(1))
+
+			// get old master sts
+			createMasterStatefulSet := &appsv1.StatefulSet{}
+			Eventually(func() error {
+				masterName := types.NamespacedName{Name: "hbasemaster", Namespace: namespace}
+				return k8sClient.Get(ctx, masterName, createMasterStatefulSet)
+			}, timeout, interval).Should(Succeed())
+			oldMasterAnnotation := createMasterStatefulSet.Annotations["hbase-controller-revision"]
+
+			// get old rs sts
+			createRsStatefulSet := &appsv1.StatefulSet{}
+			Eventually(func() error {
+				rsName := types.NamespacedName{Name: "regionserver", Namespace: namespace}
+				return k8sClient.Get(ctx, rsName, createRsStatefulSet)
+			}, timeout, interval).Should(Succeed())
+			oldRsAnnotation := createRsStatefulSet.Annotations["hbase-controller-revision"]
+
+			newHB := makeHBaseSpec(map[string]string{"hbase-site.xml": "conf2"})
+			hb.Spec.Config = newHB.Spec.Config
+			hb.Spec.MasterSpec.Count = 1
+			hb.Spec.RegionServerSpec.Count = 4
+			Expect(k8sClient.Update(ctx, hb)).Should(Succeed())
+
+			oldConfigMaps := existingCms
+			By("By checking HBase deployed new config map")
+			Eventually(func() ([]corev1.ConfigMap, error) {
+				configMapList := &corev1.ConfigMapList{}
+				listOpts := []client.ListOption{
+					client.InNamespace(namespace),
+					client.MatchingLabels(map[string]string{"config": "core"}),
+				}
+				if err := k8sClient.List(ctx, configMapList, listOpts...); err != nil {
+					return nil, err
+				}
+				return configMapList.Items, nil
+			}, timeout, interval).ShouldNot(Equal(oldConfigMaps))
+
+			By("By checking HBase updated master statefulset")
+			Eventually(func() (string, error) {
+				masterName := types.NamespacedName{Name: "hbasemaster", Namespace: namespace}
+				if err := k8sClient.Get(ctx, masterName, createMasterStatefulSet); err != nil {
+					return oldMasterAnnotation, err
+				}
+				masterAnnotation, ok := createMasterStatefulSet.Annotations["hbase-controller-revision"]
+				if !ok {
+					return oldMasterAnnotation, errors.New("no annotation")
+				}
+				return masterAnnotation, nil
+			}, timeout, interval).ShouldNot(Equal(oldMasterAnnotation))
+
+			By("By checking master statefulset has updated replicas")
+			Ω(int(*createMasterStatefulSet.Spec.Replicas)).Should(Equal(1))
+
+			By("By checking HBase updated regionserver statefulset")
+			Eventually(func() (string, error) {
+				rsName := types.NamespacedName{Name: "regionserver", Namespace: namespace}
+				if err := k8sClient.Get(ctx, rsName, createRsStatefulSet); err != nil {
+					return oldRsAnnotation, err
+				}
+				rsAnnotation, ok := createMasterStatefulSet.Annotations["hbase-controller-revision"]
+				if !ok {
+					return oldRsAnnotation, errors.New("no annotation")
+				}
+				return rsAnnotation, nil
+			}, timeout, interval).ShouldNot(Equal(oldRsAnnotation))
+
+			By("By checking master statefulset has updated replicas")
+			Ω(int(*createRsStatefulSet.Spec.Replicas)).Should(Equal(4))
+		})
+	})
+})
 
 var _ = AfterSuite(func() {
 	By("tearing down the test environment")
